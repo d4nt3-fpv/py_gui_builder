68576f4bc306fe0b1ec63572aa06ca614ab8f355
DroneDante <iPad18Dante@outlook.de>
2021-02-27 19:31:16 +0100

    Projektdateien hinzufügen.

---
+++ ben_py_gui_builder/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ben_py_gui_builder;
+
+namespace ben_py_gui_builder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+
+
+
+
+        }
+
+
+
+        public void create_btn()
+
+        {
+            Console.WriteLine("Button Click");
+
+            button1.Visible = true;
+
+
+            Console.WriteLine("Finished");
+
+        }
+
+
+
+        public void create_radbtn()
+
+        {
+            Console.WriteLine("RadButton Click");
+        }
+
+        public void create_label()
+
+        {
+            Console.WriteLine("label Click");
+        }
+
+
+        public void create_entry()
+
+        {
+            Console.WriteLine("entry Click");
+        }
+        
+        public void create_frame()
+
+        {
+            Console.WriteLine("frame Click");
+        }
+
+        public void create_checkbox()
+
+        {
+            Console.WriteLine("checkbox Click");
+        }
+
+
+        public void create_hscale()
+
+        {
+            Console.WriteLine("hscale Click");
+        }
+
+        public void create_vscale()
+
+        {
+            Console.WriteLine("vscale Click");
+        }
+
+        
+
+        public void create_progressbar()
+
+        {
+            Console.WriteLine("progressbar Click");
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ben_py_gui_builder/Form2.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ben_py_gui_builder;
+
+namespace ben_py_gui_builder
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+
+        
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            ben_py_gui_builder.Form1 exMeth = new Form1();
+            
+            exMeth.create_btn();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ben_py_gui_builder.Form1 exMeth = new Form1();
+
+            exMeth.create_radbtn();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            ben_py_gui_builder.Form1 exMeth = new Form1();
+
+            exMeth.create_label();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            ben_py_gui_builder.Form1 exMeth = new Form1();
+
+            exMeth.create_entry();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            ben_py_gui_builder.Form1 exMeth = new Form1();
+
+            exMeth.create_frame();
+
+
+        }
+
+
+
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            ben_py_gui_builder.Form1 exMeth = new Form1();
+
+            exMeth.create_hscale();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            ben_py_gui_builder.Form1 exMeth = new Form1();
+
+            exMeth.create_checkbox();
+
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ben_py_gui_builder.Form1 exMeth = new Form1();
+
+            exMeth.create_vscale();
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+            ben_py_gui_builder.Form1 exMeth = new Form1();
+
+            exMeth.create_progressbar();
+
+        }
+    }
+}
